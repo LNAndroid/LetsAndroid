aaa893bac948c1287c9dba5abe316235a822eb22
LetsNurture <android.letsnurture@gmail.com>
2018-07-25 10:41:57 +0530

    Face detection

---
+++ FaceDetection/app/src/main/java/com/facedetection/FaceOverlayGraphics.java
@@ -0,0 +1,113 @@
+package com.facedetection;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+import com.google.android.gms.vision.face.Face;
+
+class FaceOverlayGraphics extends CameraOverlay.OverlayGraphic {
+    private static final float FACE_POSITION_RADIUS = 10.0f;
+    private static final float ID_TEXT_SIZE = 40.0f;
+    private static final float ID_Y_OFFSET = 50.0f;
+    private static final float ID_X_OFFSET = -50.0f;
+    private static final float BOX_STROKE_WIDTH = 5.0f;
+
+    private static final int COLOR_CHOICES[] = {
+        Color.BLUE,
+        Color.CYAN,
+        Color.GREEN,
+        Color.MAGENTA,
+        Color.RED,
+        Color.WHITE,
+        Color.YELLOW
+    };
+    private static int mCurrentColorIndex = 0;
+
+    private Paint mFacePositionPaint;
+    private Paint mIdPaint;
+    private Paint mBoxPaint;
+
+    private volatile Face mFace;
+    private int mFaceId;
+    private float mFaceHappiness;
+    Activity activity;
+
+    FaceOverlayGraphics(CameraOverlay overlay, Activity activity) {
+        super(overlay);
+
+        this.activity = activity;
+        mCurrentColorIndex = (mCurrentColorIndex + 1) % COLOR_CHOICES.length;
+        final int selectedColor = COLOR_CHOICES[mCurrentColorIndex];
+
+        mFacePositionPaint = new Paint();
+        mFacePositionPaint.setColor(selectedColor);
+
+        mIdPaint = new Paint();
+        mIdPaint.setColor(selectedColor);
+        mIdPaint.setTextSize(ID_TEXT_SIZE);
+
+        mBoxPaint = new Paint();
+        mBoxPaint.setColor(selectedColor);
+        mBoxPaint.setStyle(Paint.Style.STROKE);
+        mBoxPaint.setStrokeWidth(BOX_STROKE_WIDTH);
+    }
+
+    void setId(int id) {
+        mFaceId = id;
+    }
+
+    void updateFace(Face face) {
+        mFace = face;
+        postInvalidate();
+    }
+    @Override
+    public void draw(Canvas canvas) {
+        Face face = mFace;
+        if (face == null) {
+            return;
+        }
+
+        // Draws a circle at the position of the detected face, with the face's track id below.
+        float x = translateX(face.getPosition().x + face.getWidth() / 2);
+        float y = translateY(face.getPosition().y + face.getHeight() / 2);
+
+        // Draws a bounding box around the face.
+        float xOffset = scaleX(face.getWidth() / 2.0f);
+        float yOffset = scaleY(face.getHeight() / 2.0f);
+        float left = x - xOffset;
+        float top = y - yOffset;
+        float right = x + xOffset;
+        float bottom = y + yOffset;
+
+        Bitmap bmp = getResizedBitmap(BitmapFactory.decodeResource(activity.getResources(), R.drawable.face),
+                face.getHeight()+100,
+                face.getHeight()+100);
+
+        canvas.drawBitmap(bmp, left+ ID_X_OFFSET, top+ ID_X_OFFSET, null);
+
+    }
+
+    public Bitmap getResizedBitmap(Bitmap bm, float newWidth, float newHeight) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // CREATE A MATRIX FOR THE MANIPULATION
+        Matrix matrix = new Matrix();
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // "RECREATE" THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createBitmap(
+                bm, 0, 0, width, height, matrix, false);
+        bm.recycle();
+        return resizedBitmap;
+    }
+
+}
